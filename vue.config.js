e751b4608bbc7ae709007d2bcbf9c8cf6040dce3
krvpb024 <krvpb024@protonmail.com>
2019-12-26 16:31:32 +0800

    add json-ld

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
+const indexJsonLd = require('./src/jsonLd/index.json')
+// const indexAuthor = require('./src/jsonLd/indexAuthor.json')
+const indexBreadcrumbList = require('./src/jsonLd/indexBreadcrumbList.json')
 
 module.exports = {
   configureWebpack: {
@@ -9,8 +12,19 @@ module.exports = {
         entry: path.join(__dirname, 'src/sw.js'),
       }),
       new HtmlWebpackPlugin({
-        minify: true,
         template: path.join(__dirname, 'public/index.html'),
+        jsonLd:
+          `<script type="application/ld+json">${JSON.stringify(indexJsonLd)}</script>` +
+          // `<script type="application/ld+json">${JSON.stringify(indexAuthor)}</script>` +
+          `<script type="application/ld+json">${JSON.stringify(indexBreadcrumbList)}</script>`,
+        minify: {
+          collapseWhitespace: true,
+          removeComments: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          useShortDoctype: true,
+        },
       }),
     ],
   },
